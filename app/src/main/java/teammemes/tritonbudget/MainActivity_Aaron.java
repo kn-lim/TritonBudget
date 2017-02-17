358e1b59df8b7ce344fc575ba84915e94af75370
Kevin Lim <knlim95@gmail.com>
2017-02-16 20:14:39 -0800

    Fix Project Directory

---
+++ app/src/main/java/teammemes/tritonbudget/MainActivity_Aaron.java
@@ -0,0 +1,14 @@
+//package com.example.xuanruzhu.thebudgetscreenfornow;
+package teammemes.tritonbudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_Aaron extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
