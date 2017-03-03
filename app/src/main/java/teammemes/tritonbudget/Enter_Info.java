65d88e34fa6a4942c30e38e83ba0d8c4d5caebb8
maoxianxian <c7ye@ucsd.edu>
2017-03-04 02:48:59 +0800

    Update Enter_Info.java

--- app/src/main/java/teammemes/tritonbudget/Enter_Info.java
+++ app/src/main/java/teammemes/tritonbudget/Enter_Info.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.content.Context;
 import android.widget.Button;
-
+import android.content.Intent;
 import android.widget.EditText;
 import android.content.SharedPreferences;
 import android.widget.Toast;
