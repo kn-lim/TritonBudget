172705fa397a7fc4b82efcc8bc421e6bb5469cc1
Shengxiang <dannyzhou97@gmail.com>
2017-03-04 15:05:50 -0800

    menu update

--- app/src/main/java/teammemes/tritonbudget/MenuDisplayPrototype_Main.java
+++ app/src/main/java/teammemes/tritonbudget/MenuDisplayPrototype_Main.java
@@ -9,17 +9,34 @@ import java.util.Collections;
 import java.util.List;
 
 public class MenuDisplayPrototype_Main extends AppCompatActivity {
+    public MenuDataSource DS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_display_main);
         // Init top level data
+
+        DS = new MenuDataSource(this);
+
+
+        List<String> listDataHeader = new ArrayList<>();
+        String[] mItemHeaders = new String[6];
+        //String[] mItemHeaders = getResources().getStringArray(R.array.items_array_expandable_level_one);
+
+        mItemHeaders[1] = "Burger Lounge";
+        mItemHeaders[2] = "Revelle Cuisine";
+        mItemHeaders[3] = "Market 64";
+        mItemHeaders[4] = "Vertically Crafted Deli";
+        mItemHeaders[5] = "Wok";
+
+        /*
         List<String> listDataHeader = new ArrayList<>();
         String[] mItemHeaders = getResources().getStringArray(R.array.items_array_expandable_level_one);
-        Collections.addAll(listDataHeader, mItemHeaders);
+
+        */Collections.addAll(listDataHeader, mItemHeaders);
         final ExpandableListView mExpandableListView = (ExpandableListView) findViewById(R.id.expandableListView_Parent);
         if (mExpandableListView != null) {
-            MenuDisplay_ParentLevelAdapter parentLevelAdapter = new MenuDisplay_ParentLevelAdapter(this, listDataHeader);
+            MenuDisplay_ParentLevelAdapter parentLevelAdapter = new MenuDisplay_ParentLevelAdapter(this, listDataHeader, DS);
             mExpandableListView.setAdapter(parentLevelAdapter);
             // display only one expand item
 //            mExpandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
