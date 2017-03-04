17189a1ef0c748d92b8f808eba6d1303fe02dd5b
anlum01 <andylum04@gmail.com>
2017-03-03 16:46:14 -0800

    Started on Splash screen

---
+++ app/src/main/java/teammemes/tritonbudget/Splash.java
@@ -0,0 +1,13 @@
+package teammemes.tritonbudget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
