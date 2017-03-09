856414300709ed1439ef6d2ad4c8249e47675ce7
Kevin Lim <knlim95@gmail.com>
2017-03-08 18:11:55 -0800

    Added Settings/Help and Icon

---
+++ app/src/main/java/teammemes/tritonbudget/Settings.java
@@ -0,0 +1,169 @@
+package teammemes.tritonbudget;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Settings extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Nav Drawers
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    final Context context = this;
+    private Toolbar mToolbar;
+    private User usr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //Creates the toolbar to the one defined in nav_action
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Settings");
+
+        //Create the Drawer layout and the toggle
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_settings);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        //Create the navigationView and add a listener to listen for menu selections
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View navHeaderView= navigationView.getHeaderView(0);
+        TextView usrName = (TextView) navHeaderView.findViewById(R.id.header_name);
+        usrName.setText(usr.getName());
+
+        //Settings Options
+        TextView changeName = (TextView) findViewById(R.id.settings_changeName);
+        TextView changeBalance = (TextView) findViewById(R.id.settings_changeBalance);
+        TextView addDD = (TextView) findViewById(R.id.settings_addDD);
+        TextView test_load = (TextView) findViewById(R.id.settings_test_load);
+
+        //Listeners
+        changeName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Change Name");
+
+                LayoutInflater viewInflated = LayoutInflater.from(context);
+                View deductView = viewInflated.inflate(R.layout.dialog_change_name,null);
+
+                // Set up the input
+
+                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                builder.setView(deductView);
+
+                final EditText input = (EditText) deductView.findViewById(R.id.changeName_input);
+
+                // Set up the buttons
+                builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Gets the input value and then deducts the balance and updates the balances
+                        //on the Home Screen
+                        String value= input.getText().toString();
+                        usr.setName(value);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    //This method is used to listen for the user clicking the menu button, and opens
+    //the drawer up
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //This method is used to see if the back button was pressed while the drawer was open.
+    //If it is open and the back button is pressed, then close the drawer.
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // This method is used to react when the user presses one of the options in the drawer
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Gets the id of the item that was selected
+        int id = item.getItemId();
+        Intent nextScreen;
+
+        //Reacts to the item selected depending on which was pressed
+        //Creates a new Intent for the new page and starts that activity
+        switch (id) {
+            case R.id.nav_home:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, HomeScreen.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_history:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, History.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_statistics:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Statistics.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_menus:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Menu.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_settings:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            case R.id.nav_help:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Help.class);
+                nextScreen.putExtra("FROM", "Help");
+                startActivity(nextScreen);
+                return true;
+            default:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+        }
+
+    }
+}
