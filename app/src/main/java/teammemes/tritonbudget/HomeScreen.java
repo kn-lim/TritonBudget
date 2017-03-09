9e48f05967c7947aaa3792cecb4b5417cb53dd58
Kevin Lim <knlim95@gmail.com>
2017-03-08 18:03:17 -0800

    Added Icon and Started work on Settings/Help

--- app/src/main/java/teammemes/tritonbudget/HomeScreen.java
+++ app/src/main/java/teammemes/tritonbudget/HomeScreen.java
@@ -304,6 +304,18 @@ public class HomeScreen extends AppCompatActivity implements NavigationView.OnNa
                 nextScreen.putExtra("FROM", "Home");
                 startActivity(nextScreen);
                 return true;
+            case R.id.nav_settings:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Settings.class);
+                nextScreen.putExtra("FROM", "Home");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_help:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Help.class);
+                nextScreen.putExtra("FROM", "Home");
+                startActivity(nextScreen);
+                return true;
             /* Cases for future options
             case R.id.nav_settings:
                 return false;
