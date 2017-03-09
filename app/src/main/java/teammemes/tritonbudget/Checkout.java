1aead32b2df12f0772c1a229f01749cec7b35933
anlum01 <andylum04@gmail.com>
2017-03-08 19:03:17 -0800

    Author: Andy Lum Message: Fixed them compiling errors.

--- app/src/main/java/teammemes/tritonbudget/Checkout.java
+++ app/src/main/java/teammemes/tritonbudget/Checkout.java
@@ -1,36 +1,25 @@
 package teammemes.tritonbudget;
-
-import teammemes.tritonbudget.db.TranHistory;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
-
 import java.util.ArrayList;
-
+import teammemes.tritonbudget.db.TranHistory;
 public class Checkout extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         //Uses custom adapter to populate list view
         populateCOList();
     }
-
     private void populateCOList() {
-
-        Bundle extras = getIntent().getExtras();
-         ArrayList<TranHistory> trans = (ArrayList) extras.getSerializable("Transactions");
-
+        Bundle extras = this.getIntent().getExtras();
+        ArrayList<TranHistory> trans = (ArrayList<TranHistory>) extras.getSerializable("Transactions");
         Checkout_Adapter co_adapter = new Checkout_Adapter(this, trans);
         ListView CO_View = (ListView) findViewById(R.id.ItemsContainer);
-
         CO_View.setAdapter(co_adapter);
-
     }
 }
