bd16b1459955210c1e113b133541a117b096f522
anlum01 <andylum04@gmail.com>
2017-03-10 15:03:36 -0800

    Author: Andy Lum 1) Nothing

--- app/src/main/java/teammemes/tritonbudget/Menus/MenuDisplay_CustomExpListView.java
+++ app/src/main/java/teammemes/tritonbudget/Menus/MenuDisplay_CustomExpListView.java
@@ -5,7 +5,7 @@ import android.widget.ExpandableListView;
 
 
 public class MenuDisplay_CustomExpListView extends ExpandableListView {
-    private final int CHILDSUB_WIDTH = 1050;
+    private final int CHILDSUB_WIDTH = 1100;
     private final int CHILDSUB_HEIGHT = 20000;
     public MenuDisplay_CustomExpListView(Context context) {
         super(context);
