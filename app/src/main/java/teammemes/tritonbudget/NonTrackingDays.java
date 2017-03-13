a94c0366fe87af66a02943ffc7313215f3d205a6
Andy Lum <andylum04@gmail.com>
2017-03-12 19:18:44 -0700

    Added nav

--- app/src/main/java/teammemes/tritonbudget/NonTrackingDays.java
+++ app/src/main/java/teammemes/tritonbudget/NonTrackingDays.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
@@ -26,30 +34,59 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import teammemes.tritonbudget.db.HistoryDataSource;
 import teammemes.tritonbudget.db.TranHistory;
 
 import static android.R.attr.id;
 import static android.view.Gravity.CENTER;
 import static android.view.Gravity.RIGHT;
 
-public class NonTrackingDays extends Activity {
+public class NonTrackingDays extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ArrayList<String> noneatingdays = new ArrayList<>();
     private DatePicker datePicker;
     private Calendar calendar;
     private int inityear, initmonth, initday;
-    private User usr;
     int id = 100;
     LinearLayout.LayoutParams layoutParams, textParams, btnParams, costParams;
     LinearLayout mainLayout;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private Toolbar mToolbar;
+    private User usr;
+    private TextView usrName;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nontracking);
+        setContentView(R.layout.drawer_nontracking);
 
         usr = User.getInstance(getApplicationContext());
 
+        /*Navigation Drawer stuff*/
+        //Creates the toolbar to the one defined in nav_action
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Settings");
+
+        //Create the Drawer layout and the toggle
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_nontracking);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        //Create the navigationView and add a listener to listen for menu selections
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View navHeaderView= navigationView.getHeaderView(0);
+        usrName = (TextView) navHeaderView.findViewById(R.id.header_name);
+        usrName.setText(usr.getName());
+
+
+        /* Begin Date Picker Stuff*/
         calendar = Calendar.getInstance();
         inityear = calendar.get(Calendar.YEAR);
         initmonth = calendar.get(Calendar.MONTH);
@@ -61,14 +98,7 @@ public class NonTrackingDays extends Activity {
         btnParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);  //was (200,100)
         btnParams.setMargins(0, 0, 0, 0); //was (0,0,0,40)
 
-        /*
-        TextView test = (TextView) findViewById(R.id.NED_TV_LIST);
-        noneatingdays = usr.getNon_tracking_days();
-        Collections.sort(noneatingdays);
-        for (int i = 0; i < noneatingdays.size(); i++) {
-            test.append(" " + noneatingdays.get(i));
-        }
-        */
+
         //Collections.sort(usr.getNon_tracking_days());
         render_non_eating(usr.getNon_tracking_days());
         datePicker = (DatePicker) findViewById(R.id.datePicker);
@@ -109,6 +139,76 @@ public class NonTrackingDays extends Activity {
         });
     }
 
+    //This method is used to listen for the user clicking the menu button, and opens
+    //the drawer up
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //This method is used to see if the back button was pressed while the drawer was open.
+    //If it is open and the back button is pressed, then close the drawer.
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // This method is used to react when the user presses one of the options in the drawer
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Gets the id of the item that was selected
+        int id = item.getItemId();
+        Intent nextScreen;
+
+        //Reacts to the item selected depending on which was pressed
+        //Creates a new Intent for the new page and starts that activity
+        switch (id) {
+            case R.id.nav_home:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, HomeScreen.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_history:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, History.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_statistics:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Statistics.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_menus:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, DiningHallSelection.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            case R.id.nav_settings:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            case R.id.nav_help:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                nextScreen = new Intent(this, Help.class);
+                nextScreen.putExtra("FROM", "Settings");
+                startActivity(nextScreen);
+                return true;
+            default:
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+        }
+    }
+
     private void showDate(int year, int month, int day) {
         String str = " " + new StringBuilder().append(month).append("/").append(day).append("/").append(year);
         Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
@@ -131,8 +231,7 @@ public class NonTrackingDays extends Activity {
         noneatingdays = usr.getNon_tracking_days();
         if (!noneatingdays.contains(date_noeat)) {
             usr.setNon_tracking_days(date_noeat);
-            //showDate(curryear, monthOfYear + 1, dayOfMonth);
-            //Collections.sort(noneatingdays);
+
             render_non_eating(noneatingdays);
         } else {
             Toast.makeText(getApplicationContext(), "Already accounted for!", Toast.LENGTH_SHORT).show();
@@ -244,7 +343,7 @@ public class NonTrackingDays extends Activity {
         }
     }
 
-    public void removeAllButtons(){
+    private void removeAllButtons(){
         int numTrans = mainLayout.getChildCount();
         for (int j = 0; j < numTrans; j++){
             LinearLayout tran = (LinearLayout) mainLayout.getChildAt(j);
