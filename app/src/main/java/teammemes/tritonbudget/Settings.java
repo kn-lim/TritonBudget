5e6a45ac55f9915ec114f3fa9a066428f1416be0
David <dtallman@ucsd.edu>
2017-03-14 13:34:01 -0700

    Added the test data. Needs some tweaking but kinda works.

--- app/src/main/java/teammemes/tritonbudget/Settings.java
+++ app/src/main/java/teammemes/tritonbudget/Settings.java
@@ -22,7 +22,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 import teammemes.tritonbudget.db.HistoryDataSource;
 import teammemes.tritonbudget.db.TranHistory;
@@ -37,6 +42,7 @@ public class Settings extends AppCompatActivity implements NavigationView.OnNavi
     private User usr;
     private HistoryDataSource database;
     private TextView usrName;
+    SimpleDateFormat dateFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +51,7 @@ public class Settings extends AppCompatActivity implements NavigationView.OnNavi
         usr = User.getInstance(getApplicationContext());
 
         database = new HistoryDataSource(this);
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
         //Creates the toolbar to the one defined in nav_action
         mToolbar = (Toolbar) findViewById(R.id.nav_action);
@@ -265,33 +272,40 @@ public class Settings extends AppCompatActivity implements NavigationView.OnNavi
         test_load.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setTitle("What's Your Name?");
-
-                LayoutInflater viewInflated = LayoutInflater.from(context);
-                View deductView = viewInflated.inflate(R.layout.dialog_change_name,null); //TODO: CHANGE THIS
-
                 // Set up the input
+                List<TranHistory> trans = database.getAllTransaction();
+                for (int i = 0; i < trans.size(); i++){
+                    database.deleteTransaction(trans.get(i).getId());
+                }
 
-                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                builder.setView(deductView);
+                Calendar start = Calendar.getInstance();
+                Calendar today = (Calendar) start.clone();
+                start.set(2017,0,1);
 
-                final EditText input = (EditText) deductView.findViewById(R.id.changeName_input); //TODO: CHANGE THIS
-
-                // Set up the buttons
-                builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //TODO: CHANGE THIS
+                Random rand = new Random();
+                int count = 0;
+                do{
+                    if (start.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY){
+                        start.add(Calendar.DATE, 2);
                     }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
+                    if (start.after(today)){
+                        break;
                     }
-                });
-                builder.show();
+
+                    if (count < 3){
+                        double cost = rand.nextDouble();
+                        cost = Math.round(cost*1000)/100;
+                        TranHistory tran = new TranHistory(1, "Test Item",1,start.getTime(), cost);
+                        database.createTransaction(tran);
+                        count++;
+                    }
+                    else{
+                        count = 0;
+                        start.add(Calendar.DATE, 1);
+                    }
+                }while (true);
+
+                Toast.makeText(context,"Loaded in test Data", Toast.LENGTH_LONG);
             }
         });
 
